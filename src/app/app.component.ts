327399c6cc59ddedc7b73f7e14d894385125e068
Jesaiah Prayor <JPrayor@dcas.nyc.gov>
2019-03-20 14:47:49 -0400

    heroes angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// Get user input and add it into the heroes array;
+
 export class AppComponent {
-  title = 'my-app';
+
+  title = 'Fallen Heroes';
+  heroes = ['Bruce Wayne', ' Jimmy Carter ', ' Jimmy Nuetron '];
 }
+
